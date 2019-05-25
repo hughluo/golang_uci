341fee40ed7b4f8dd8fde281d4b7d884965f8685
Wexort <wexort.com@gmail.com>
2019-05-25 10:47:56 +0200

    change interface

--- Functions, Methods, and Interfaces in Go/week4/animals_interface.go
+++ Functions, Methods, and Interfaces in Go/week4/animals_interface.go
@@ -1,40 +1,40 @@
 /* Write a program which allows the user to create a set of animals and to get information about those animals.
- * Each animal has a name and can be either a cow, bird, or snake. 
- * With each command, the user can either create a new animal of one of the three types, 
- * or the user can request information about an animal that he/she has already created. 
- * Each animal has a unique name, defined by the user. Note that the user can define animals of a chosen type, 
- * but the types of animals are restricted to either cow, bird, or snake. 
+ * Each animal has a name and can be either a cow, bird, or snake.
+ * With each command, the user can either create a new animal of one of the three types,
+ * or the user can request information about an animal that he/she has already created.
+ * Each animal has a unique name, defined by the user. Note that the user can define animals of a chosen type,
+ * but the types of animals are restricted to either cow, bird, or snake.
  * The following table contains the three types of animals and their associated data.
 
  * Animal	Food eaten	Locomotion method	Spoken sound
  * cow	        grass	        walk	           	moo
  * bird	        worms	        fly	                peep
  * snake	mice	        slither	                hsss
- 
- * Your program should present the user with a prompt, “>”, to indicate that the user can type a request. 
- * Your program should accept one command at a time from the user, print out a response, and print out a new prompt on a new line. 
+
+ * Your program should present the user with a prompt, “>”, to indicate that the user can type a request.
+ * Your program should accept one command at a time from the user, print out a response, and print out a new prompt on a new line.
  * Your program should continue in this loop forever. Every command from the user must be either a “newanimal” command or a “query” command.
- 
- * Each “newanimal” command must be a single line containing three strings. 
- * The first string is “newanimal”. 
- * The second string is an arbitrary string which will be the name of the new animal. 
- * The third string is the type of the new animal, either “cow”, “bird”, or “snake”. 
+
+ * Each “newanimal” command must be a single line containing three strings.
+ * The first string is “newanimal”.
+ * The second string is an arbitrary string which will be the name of the new animal.
+ * The third string is the type of the new animal, either “cow”, “bird”, or “snake”.
  * Your program should process each newanimal command by creating the new animal and printing “Created it!” on the screen.
 
- * Each “query” command must be a single line containing 3 strings. 
- * The first string is “query”. 
- * The second string is the name of the animal. 
- * The third string is the name of the information requested about the animal, either “eat”, “move”, or “speak”. 
+ * Each “query” command must be a single line containing 3 strings.
+ * The first string is “query”.
+ * The second string is the name of the animal.
+ * The third string is the name of the information requested about the animal, either “eat”, “move”, or “speak”.
  * Your program should process each query command by printing out the requested data.
- 
- * Define an interface type called Animal which describes the methods of an animal. 
- * Specifically, the Animal interface should contain the methods Eat(), Move(), and Speak(), which take no arguments and return no values. 
- * The Eat() method should print the animal’s food, the Move() method should print the animal’s locomotion, 
- * and the Speak() method should print the animal’s spoken sound. 
- * Define three types Cow, Bird, and Snake. 
- * For each of these three types, define methods Eat(), Move(), and Speak() 
- * so that the types Cow, Bird, and Snake all satisfy the Animal interface. 
- * When the user creates an animal, create an object of the appropriate type. 
+
+ * Define an interface type called Animal which describes the methods of an animal.
+ * Specifically, the Animal interface should contain the methods Eat(), Move(), and Speak(), which take no arguments and return no values.
+ * The Eat() method should print the animal’s food, the Move() method should print the animal’s locomotion,
+ * and the Speak() method should print the animal’s spoken sound.
+ * Define three types Cow, Bird, and Snake.
+ * For each of these three types, define methods Eat(), Move(), and Speak()
+ * so that the types Cow, Bird, and Snake all satisfy the Animal interface.
+ * When the user creates an animal, create an object of the appropriate type.
  * Your program should call the appropriate method when the user issues a query command.
  */
 package main
@@ -49,11 +49,7 @@ import (
 func main() {
 
 	// A map that key is animal name and value is animal type
-	my_map := new(AllMap)
-	my_map.cow = make(map[string]Cow)
-	my_map.bird = make(map[string]Bird)
-	my_map.snake = make(map[string]Snake)
-	my_map.animal = make(map[string]string)
+	animal_map := make(map[string]Animal)
 
 	reader := bufio.NewReader(os.Stdin)
 	for {
@@ -71,9 +67,9 @@ func main() {
 
 		switch command {
 		case "newanimal":
-			NewAnimal(arg_1, arg_2, my_map)
+			NewAnimal(arg_1, arg_2, animal_map)
 		case "query":
-			Query(arg_1, arg_2, my_map)
+			Query(arg_1, arg_2, animal_map)
 		default:
 			fmt.Printf("Command \"%v\" not found. Please try again\n", command)
 		}
@@ -81,47 +77,31 @@ func main() {
 
 }
 
-func NewAnimal(a_name string, a_type string, my_map *AllMap) {
+func NewAnimal(a_name string, a_type string, animal_map map[string]Animal) {
 	//new_animal := new(Animal)
 	switch a_type {
 	case "cow":
-		my_map.animal[a_name] = "cow"
-		my_map.cow[a_name] = *new(Cow)
+		animal_map[a_name] = *new(Cow)
 	case "bird":
-		my_map.animal[a_name] = "bird"
-		my_map.bird[a_name] = *new(Bird)
+		animal_map[a_name] = *new(Bird)
 	case "snake":
-		my_map.animal[a_name] = "snake"
-		my_map.snake[a_name] = *new(Snake)
+		animal_map[a_name] = *new(Snake)
 	default:
 		fmt.Printf("Animal type \"%v\" not found. Please try again\n", a_type)
 	}
 
 }
 
-/* func CreateNewAnimal(a Animal, a_name string, animal_map map[string]string){
-	animal_map[a_name] = a
-	fmt.Println("Created it!")
-} */
-
-func Query(a_name string, info string, my_map *AllMap) {
-	if a_type, ok := my_map.animal[a_name]; ok {
-		switch a_type {
-		case "cow":
-			GetInfo(my_map.cow[a_name], info)	
-		case "bird":
-			GetInfo(my_map.bird[a_name], info)	
-		case "snake":
-			GetInfo(my_map.snake[a_name], info)	
-		}
+func Query(a_name string, info string, animal_map map[string]Animal) {
+	if animal_obj, ok := animal_map[a_name]; ok {
+		GetInfo(animal_obj, info)
 	} else {
 		fmt.Printf("Animal name \"%v\" not found. Please try again\n", a_name)
 	}
 }
 
-
 func GetInfo(a Animal, info string) {
-    switch info {
+	switch info {
 	case "eat":
 		a.Eat()
 	case "move":
@@ -134,35 +114,30 @@ func GetInfo(a Animal, info string) {
 }
 
 type AllMap struct {
-	cow map[string]Cow
-	bird map[string]Bird
-	snake map[string]Snake
+	cow    map[string]Cow
+	bird   map[string]Bird
+	snake  map[string]Snake
 	animal map[string]string
 }
 
-
 type Animal interface {
-	Eat() 
+	Eat()
 	Move()
 	Speak()
 }
 
 type Cow struct {
-
 }
 
 type Bird struct {
-	
 }
 
 type Snake struct {
-
 }
 
 func (c Cow) Eat() {
 	fmt.Println("grass")
 }
-
 
 func (c Cow) Move() {
 	fmt.Println("walk")
